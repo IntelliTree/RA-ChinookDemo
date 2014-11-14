43c7cc3cb0ef7668d41fdceb31100a3f5c1c14dc
Henry Van Styn <vanstyn@intellitree.com>
2014-11-14 17:14:16 -0500

    Added an example multi-level has_many

--- lib/RA/ChinookDemo/DB/Result/Employee.pm
+++ lib/RA/ChinookDemo/DB/Result/Employee.pm
@@ -74,6 +74,26 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07036 @ 2013-09-12 15:36:29
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:wHFFXU19HU2txd7Slmw1vg
 
+# Example of a multi-level join rel (Customer in-between)
+__PACKAGE__->has_many(
+  "invoices",
+  "RA::ChinookDemo::DB::Result::Invoice",
+  sub {
+    my $args = shift;
+    
+    my $MiddleRsCol = $args->{self_resultsource}->schema->resultset('Customer')
+      ->search_rs(undef,{ alias => 'cust_alias' })
+      ->search_rs({ 
+        'cust_alias.supportrepid' => { -ident => "$args->{self_alias}.employeeid" }
+      })
+      ->get_column('customerid');
+      
+    return (
+      { "$args->{foreign_alias}.customerid" => { -in => $MiddleRsCol->as_query } }
+    );
+  }
+);
+
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
